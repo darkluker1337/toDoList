f431a4dde7fb4a847a4e43a8071d344355fa345e
Pavel Emelyanov <lictratenkodenis26@gmail.com>
2022-11-15 19:31:05 +0300

    app change

--- src/core/Component/Component.js
+++ src/core/Component/Component.js
@@ -1,48 +1,38 @@
 export class Component extends HTMLElement {
-  constructor() {
-    super();
-    this.state = {};
-    this.props = {};
+  constructor(){
+   super()
+   this.props = {};
+   this.state = {};
+}
+
+ setState(callback){
+   this.state = callback(this.state);
+   this.innerHTML = this.render();
+ }
+
+connectedCallback(){
+ this.innerHTML = this.render();
+ this.componentDidMount();
+}
+
+ disconnectedCallback(){
+  this.componentWillUnmount()
+ }
+
+ attributeChangedCallback(name, oldValue, newValue){
+  this.componentWillUpdate(name, oldValue, newValue);
+  this.getAttributeNames().forEach((name)=>{
+    this.props[name] = this.getAttribute(name)
+  })
+ }
+
+
+  dispatch(type, props){
+    this.dispatchEvent(new CustomEvent(type, {bubbles:true, detail:props}))
   }
 
-  setState(callback) {
-    this.state = callback(this.state);
-    this.innerHTML = this.render()
-      .toString()
-      .replaceAll(",", "")
-      .trim()
-      .replaceAll(/true|false/gi, "");
-  }
-
-  connectedCallback() {
-    this.innerHTML = this.render()
-      .toString()
-      .replaceAll(",", "")
-      .trim()
-      .replaceAll(/true|false/gi, "");
-    this.componentDidMount();
-    this.registerEvents();
-  }
-
-  disconnectedCallback() {
-    this.componentWillUnmount();
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    this.componentWillUpdate(name, oldValue, newValue);
-    this.getAttributeNames().forEach((name) => {
-      this.props[name] = this.getAttribute(name);
-    });
-  }
-
-  dispatch(type, props) {
-    this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }));
-  }
-
-  registerEvents() {}
-  componentDidMount() {}
-  componentWillUnmount() {}
-  componentWillUpdate() {}
-  componentWillUnmount() {}
-  render() {}
+ render(){}
+ componentDidMount(){};
+ componentWillUnmount(){};
+ componentWillUpdate(){};
 }
