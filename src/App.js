8f13857b44567af01f5d718f41210503e19c86e6
Pavel Emelyanov <lictratenkodenis26@gmail.com>
2022-11-18 21:29:42 +0300

    add read tasks

--- src/App.js
+++ src/App.js
@@ -4,13 +4,51 @@ import './components/moleculs/InputGroup/InputGroup'
 
 export class App extends Component {
 
-
+  constructor(){
+    super()
+    this.state = {
+      tasks: []
+    }
+  }
+  
+  componentDidMount(){
+    todoList.getTasks().then((data)=>{
+      this.setState((state)=>{
+        return {
+          ...state,
+          tasks: data,
+      }
+      })
+      
+    })
+  }
 
   render() {
     return `
       <div class='container mt-5'>
         <my-input-group></my-input-group>
       </div>
+      <ul class="list-group">
+        ${this.state.tasks.map((task)=>{
+        return`
+            <li class="list-group-item">
+            <div class="form-check d-flex justify-content-between align-items-center">
+              <div>
+                  <input class="form-check-input" type="checkbox" ${task.isCompleted ? 'checked' : ''} value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                     ${task.title}
+                  </label>
+                </div>
+                <div class='d-flex'>
+                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                  <button type="button" class="btn btn-primary btn-sm">Update</button>
+                </div>
+            </div>
+          </li>
+            `
+        
+        }).join(' ')}
+      </ul>
         `
   }
 }
