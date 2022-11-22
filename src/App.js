496a6f213e31d1f29cbbcf005c86c5e946068800
Pavel Emelyanov <lictratenkodenis26@gmail.com>
2022-11-22 21:30:36 +0300

    add delete

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
 import { Component } from "./core";
 import {todoList} from './services/todoList/TodoList'
 import './components/moleculs/InputGroup/InputGroup'
+import './components/moleculs/Task/Task'
 
 export class App extends Component {
 
   constructor(){
     super()
     this.state = {
-      tasks: []
+      tasks: [],
+      isLoading:false,
     }
   }
   
-  componentDidMount(){
+  getTasks(){
     todoList.getTasks().then((data)=>{
       this.setState((state)=>{
         return {
           ...state,
-          tasks: data,
+          tasks: data.map((item)=>({...item, isEditting:false})),
       }
       })
       
     })
   }
+  
+  
+  saveTask = (evt) =>{
+    todoList.createTask({...evt.detail, isCompleted:false})
+      .then(()=>{
+        this.getTasks()
+      })
+  }
+  
+  deleteTask = (id) => {
+    todoList.deleteTask(id)
+    .then(()=>{
+      this.getTasks()
+    })
+  }
+
+  onClick =(evt)=>{
+    const target = evt.target;
+    if(target.closest('.delete-action')){
+      const data = target.dataset // нужен для data-id
+      this.deleteTask(data.id)   
+    }
+  }
+
+  componentDidMount(){
+    this.getTasks()
+    this.addEventListener('save-task',this.saveTask)
+    this.addEventListener('click',this.onClick)
+
+  }
+  componentWillUnmount(){
+    this.removeEventListener('save-task',this.saveTask)
+    this.removeEventListener('click',this.onClick)
+
+  }
 
   render() {
     return `
       <div class='container mt-5'>
-        <my-input-group></my-input-group>
+        <my-input-group type='save-task'></my-input-group>
       </div>
       <ul class="list-group">
         ${this.state.tasks.map((task)=>{
         return`
-            <li class="list-group-item">
-            <div class="form-check d-flex justify-content-between align-items-center">
-              <div>
-                  <input class="form-check-input" type="checkbox" ${task.isCompleted ? 'checked' : ''} value="" id="flexCheckDefault">
-                  <label class="form-check-label" for="flexCheckDefault">
-                     ${task.title}
-                  </label>
-                </div>
-                <div class='d-flex'>
-                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                  <button type="button" class="btn btn-primary btn-sm">Update</button>
-                </div>
-            </div>
-          </li>
+            <my-task id="${task.id}" title="${task.title}" iscompleted="${task.isCompleted}"></my-task>
             `
         
         }).join(' ')}
--- src/components/moleculs/InputGroup/InputGroup.js
+++ src/components/moleculs/InputGroup/InputGroup.js
@@ -1,88 +1,53 @@
 import { Component } from "../../../core";
-import "../../atomcs/Button/Button"
-import "../../atomcs/Input/Input"
-import { todoList } from "../../../services/todoList/TodoList";
-import "../../atomcs/Spinner/Spinner"
-import "./InpuGroup.scss"
-export class InputGroup extends Component{
- 
-   constructor(){
-     super();
-    
-     this.state = {
-       inputValue: '',
-       isloading:false,
-       error:''
-     }
-   }
-   
-   onSave(){
-     if(this.state.inputValue){
-        this.setState((state)=>{
-          return{
-            ...state,
-            isloading:true
-          }
-        })
-       todoList.createTask({
-         title: this.state.inputValue,
-         isCompleted: false,
-       }).then(()=>{
-          this.setState((state)=>{
-            return{
-              ...state,
-              isloading:false,
-              inputValue:'',
-            }
-         })
-         throw new Error('Ошибочка')
-       })
-       .catch((e)=>{
-          this.setState((state)=>{
-            return{
-              ...state,
-              error: e.message
-            }
-          })
-       })
-       .finally(()=>{
-        this.setState((state)=>{
-          return{
-            ...state,
-            isloading:false,
-            inputValue:'',
-          }
-       })
-     })
-     }
-   }
-   
-   onInput(evt){
-    this.setState((state)=>{
-     return{
-      ...state,
-      inputValue: evt.detail.value
-     }
-    })
-   }
-   
-   componentDidMount(){
-      this.addEventListener('save-task',this.onSave)
-      this.addEventListener('custom-input', this.onInput)
-   }
 
-   render(){
+import { todoList } from "../../../services/todoList/TodoList";
+import "./InpuGroup.scss"
+export class InputGroup extends Component {
+
+
+
+  onSumbit = (evt) => {
+    evt.preventDefault()
+    const data = new FormData(evt.target);
+    const task = {}
+    data.forEach((value,key)=>{
+      task[key] = value;
+    })
+    console.log(task)
+    this.dispatch(this.props.type,task)
+  }
+
+  componentDidMount() {
+    this.addEventListener('submit', this.onSumbit)
+  }
+  componentWillUnmount() {
+    this.removeEventListener('submit', this.onSumbit)
+  }
+
+  static get observedAttributes(){
+    return ['type','cancel']
+  }
+
+  render() {
     return `
-    <div>${this.state.error}</div>
-    <div class="input-group mb-3">
-      <my-input value="${this.state.inputValue}" placeholder="Add a new task" type="text"></my-input>
-      <my-button eventtype='save-task' content="save" class="btn btn-outline-primary" type="button" id="button-addon2"></my-button>
-    </div>
-    <my-spinner class ="${this.state.isloading ? 'load' : 'notload'}"></my-spinner>
+    <form class="input-group mb-3">
+      <input
+      cansel=""
+      name="title"
+      type='text'
+      class='form-control'
+      placeholder='Add new task'
+      >
+      <button type='submit' class="btn btn-outline-primary">Save</button>
+    </form>
+  
     
    `
-   
-   }
+
+  }
 }
 
-customElements.define('my-input-group',InputGroup)
+customElements.define('my-input-group', InputGroup)
+
+{/* <div>${this.state.error}</div> */ }
+// {<my-spinner class ="${this.state.isloading ? 'load' : 'notload'}"></my-spinner>}
